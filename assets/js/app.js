99455eac22263d4037ef9607416bd0f429e7e0c1
jatin-rajput563 <jkt3298@gmail.com>
2025-03-17 11:20:25 +0530

    background-change

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,11 +6,17 @@ function inputBtn() {
     let inputValue = document.getElementById("input").value;
     alert(inputValue);
 }
+
 function createBtn() {
     let newParagraph = document.createElement("p");
     newParagraph.textContent = "This is a new paragraph added to the document.";
     document.body.appendChild(newParagraph);
 }
+
 function removeBtn() {
     document.getElementById("removeText").style.display = "none";
+}
+
+window.onload = function () {
+    document.body.style.backgroundColor = "yellow";
 }
