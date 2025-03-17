4e86b4ef3730edf62764c04fc7a680a7b9c4a87b
jatin-rajput563 <jkt3298@gmail.com>
2025-03-17 11:05:28 +0530

    remote element

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,11 @@ function inputBtn() {
     let inputValue = document.getElementById("input").value;
     alert(inputValue);
 }
-function paraFunction() {
+function createBtn() {
     let newParagraph = document.createElement("p");
     newParagraph.textContent = "This is a new paragraph added to the document.";
     document.body.appendChild(newParagraph);
-    
+}
+function removeBtn() {
+    document.getElementById("removeText").style.display = "none";
 }
